5e09bd78b0d4ce32714665695ac3170e86a25c5e
Zachary Haber <zachary.haber@abbvie.com>
2022-06-13 12:10:36 -0500

    chore: Improve type declaration

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,18 +1,24 @@
 export interface LogFacesHTTPAppender {
-        type: '@log4js-node/logfaces-http';
-        // logFaces receiver servlet URL
-        url: string;
-        // (defaults to empty string) - used to identify your application’s logs
-        application?: string;
-        // (defaults to 5000ms) - the timeout for the HTTP request.
-        timeout?: number;
-        /** logFaces hostname (h) property */
-        hostname?: string;
+  type: "@log4js-node/logfaces-http";
+  /** logFaces receiver servlet URL */
+  url: string;
+  /** used to identify your application’s logs
+   * @default ''
+   * */
+  application?: string;
+  /** the timeout for the HTTP request.
+   * @default 5000ms
+   * */
+  timeout?: number;
+  /** logFaces hostname (h) property */
+  hostname?: string;
+  /** The shared global config to include in your logs */
+  configContext?: () => Record<string, string | number>;
 }
 
 // Add the LogFacesHTTPAppender to the list of appenders in log4js for better type support
-declare module 'log4js'{
-        export interface Appenders {
-                LogFacesHTTPAppender: LogFacesHTTPAppender
-        }
+declare module "log4js" {
+  export interface Appenders {
+    LogFacesHTTPAppender: LogFacesHTTPAppender;
+  }
 }
